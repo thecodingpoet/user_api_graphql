3a842c2ebb2fba8f29cd079b8fb677a64ac7f56c
Tomi Duyile <thecodingpoet@gmail.com>
2022-07-25 14:36:46 +0100

    Setup graphQL

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String
+    field :last_name, String
+    field :email, String, null: false
+  end
+end
